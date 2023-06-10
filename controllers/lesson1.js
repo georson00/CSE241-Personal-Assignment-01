883b11c071a0e30e2c162c5f296da23f0a552c4e
Nelson Georges <georson00@gmail.com>
2023-06-09 21:24:09 -0400

    adding controller

---
+++ controllers/lesson1.js
@@ -0,0 +1,9 @@
+const karolaneRoute = (req, res)=> {
+    res.send('Karolane G. Georges')};
+
+
+    
+    module.exports = {
+      karolaneRoute, 
+     
+    };
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res)=> {
-    res.send('Karolane G. Georges');
-});
-app.listen(3000);
+const lesson1Controller = require('./controllers/lesson1');
+
+
+app.get('/', lesson1Controller.karolaneRoute);
+
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || port));
